6f0b96300deecceb0442ecda7be553449a8dc575
George Pavlos <120208246+geo14150@users.noreply.github.com>
2025-10-31 01:30:58 +0200

    Update password.py

--- password.py
+++ password.py
@@ -9,7 +9,9 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-for char in range(0, nr_letters):
+password_list = []
+
+for char in range(0, nr_letters + 1):
     password_list.append(random.choice(letters))
 for char in range(0, nr_symbols):
     password_list.append(random.choice(symbols))
